f521c793eb27418327440ef172a6bb7c2ab1dac5
ConnorX <connor_d_hall@hotmail.com>
2015-10-07 23:21:24 -0400

    fourth commit
    more views, css, information

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',
-                      message: 'First Page'});
+  res.render('index', { title: 'Connor Hall Portfolio',
+                      message: 'My Portfolio'});
 });
 
 /* GET ABOUT page */
 router.get('/about', function(req, res, next) {
    res.render('about'); 
+});
+
+/* GET Education page */
+router.get('/education', function(req, res, next) {
+   res.render('education'); 
+});
+
+/* GET Qualities page */
+router.get('/qualities', function(req, res, next) {
+   res.render('qualities'); 
+});
+
+/* GET Work Expierence page */
+router.get('/workexpierence', function(req, res, next) {
+   res.render('workexpierence'); 
+});
+
+/* GET Safety page */
+router.get('/safety', function(req, res, next) {
+   res.render('safety'); 
+});
+
+/* GET Volunteer Expierence page */
+router.get('/volunteerexpierence', function(req, res, next) {
+   res.render('volunteerexpierence'); 
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+    
+    // create a user list
+    var userList = ['Me', 'You', 'Them'];
+    
+    //load a users view instead
+    res.render('users', { userList: userList } );
 });
 
 module.exports = router;
